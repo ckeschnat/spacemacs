296292b3648dad5e4b90bb2926e0d0dcfb8faa49
Chris Keschnat <chris@rivered.de>
2018-10-08 16:29:23 +0200

    add go layer

--- init.el
+++ init.el
@@ -38,6 +38,7 @@ values."
      ruby
      asciidoc
      themes-megapack
+     go
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or

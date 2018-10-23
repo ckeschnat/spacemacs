bd17869f6fffedcf5bac0a9173214705f1ec0ab7
Chris Keschnat <chris@rivered.de>
2018-10-23 15:54:41 +0200

    org export background colors

--- chris-org.el
+++ chris-org.el
@@ -2,6 +2,26 @@
 
 (with-eval-after-load 'org
     ;; here goes your Org config :)
+    ;; background colors for export http://ivanmalison.github.io/dotfiles/#setbackgroundcolorofsourceblocksforexport
+    (use-package org
+      :config
+      (progn
+        (defun imalison:org-inline-css-hook (exporter)
+          "Insert custom inline css to automatically set the
+  background of code to whatever theme I'm using's background"
+          (when (eq exporter 'html)
+            (let* ((my-pre-bg (face-background 'default))
+                  (my-pre-fg (face-foreground 'default)))
+              (setq
+              org-html-head-extra
+              (concat
+                org-html-head-extra
+                (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                        my-pre-bg my-pre-fg))))))
+
+        (add-hook 'org-export-before-processing-hook 'imalison:org-inline-css-hook)))
+
+
     (load "~/.spacemacs.d/org-archive-subtree-hierarchical")
 
     (setq org-startup-indented t)

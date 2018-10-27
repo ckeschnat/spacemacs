8bef906df0ae96a9e683f8d7b47779b160f80f90
Chris Keschnat <chris@rivered.de>
2018-10-27 12:38:24 +0200

    org agenda stuff

--- chris-org.el
+++ chris-org.el
@@ -21,6 +21,8 @@
 
         (add-hook 'org-export-before-processing-hook 'imalison:org-inline-css-hook)))
 
+    (evil-define-key 'evilified org-agenda-mode-map (kbd "j") 'org-agenda-next-item)
+    (evil-define-key 'evilified org-agenda-mode-map (kbd "k") 'org-agenda-previous-item)
 
     (load "~/.spacemacs.d/org-archive-subtree-hierarchical")
 
@@ -89,6 +91,49 @@
                   ("TODO" ("WAITING") ("CANCELLED"))
                   ("NEXT" ("WAITING") ("CANCELLED"))
                   ("DONE" ("WAITING") ("CANCELLED")))))
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;; https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+    ;; maybe use https://github.com/alphapapa/org-super-agenda
+
+    (defun air-org-skip-subtree-if-priority (priority)
+      "Skip an agenda subtree if it has a priority of PRIORITY.
+
+      PRIORITY may be one of the characters ?A, ?B, or ?C."
+      (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+            (pri-value (* 1000 (- org-lowest-priority priority)))
+            (pri-current (org-get-priority (thing-at-point 'line t))))
+        (if (= pri-value pri-current)
+            subtree-end
+          nil)))
+
+    ;; (setq org-agenda-custom-commands
+    ;;       '(("c" "Simple agenda view"
+    ;;          ((tags "PRIORITY=\"A\""
+    ;;                 ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+    ;;                  (org-agenda-overriding-header "High-priority unfinished tasks:")))
+    ;;           (agenda "")
+    ;;           (alltodo ""
+    ;;                    ((org-agenda-skip-function
+    ;;                      '(or (air-org-skip-subtree-if-priority ?A)
+    ;;                           (org-agenda-skip-if nil '(scheduled deadline))))))))))
+
+    (setq org-agenda-custom-commands
+          '(("d" "Daily agenda and all TODOs"
+             ((tags "PRIORITY=\"A\""
+                    ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                     (org-agenda-overriding-header "High-priority unfinished tasks:")))
+              (agenda "" ((org-agenda-span 'day)))
+              (alltodo ""
+                       ((org-agenda-skip-function '(or (air-org-skip-subtree-if-priority ?A)
+                                                       (org-agenda-skip-if nil '(scheduled deadline))))
+                        (org-agenda-overriding-header "ALL normal priority tasks:"))))
+             ((org-agenda-compact-blocks t)))))
+
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 
     ;; (setq org-agenda-custom-commands
     ;;       (quote (("N" "Notes" tags "NOTE"
